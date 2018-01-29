3b5b5c430227e45f24ae373b8b99c91cf844c079
RedSalas <j.saulff@gmail.com>
2018-01-29 00:34:44 -0600

    Automata

---
+++ Compilador/AnalizadorLexico/lexico.py
@@ -0,0 +1,56 @@
+class Lexico:
+    fuente = str
+    ind = int
+    continua = bool
+    c = str
+    estado = int
+    simbolo = str
+    tipo = str
+
+    def __init__(self,codigo): #Entrada
+        self.fuente = codigo
+        self.ind = 0
+
+    def TipoCadena(self,tipo):
+        switcher = {
+            0: "IDENTIFICADOR",
+            1: "ENTERO",
+            2: "REAL",
+            3: "CADENA",
+            4: "TIPO",
+            5: "OPSUMA",
+            6: "OPMUL",
+            7: "OPRELAC",
+            8: "OPOR",
+            9: "OPAND",
+            10: "OPNOT",
+            11: "OPIGUALDAD",
+            12: ";",
+            13: ",",
+            14: "(",
+            15: ")",
+            16: "{",
+            17: "}",
+            18: "=",
+            19: "if",
+            20: "while",
+            21: "return",
+            22: "else",
+            23: "$",
+        }
+        return switcher.get(tipo,"Error")
+
+    def CadenaToArray(self):
+        cadena = []
+        for letra in self.fuente
+            
+
+    def Automata(self):
+        while True:
+            if()
+
+f = open("entrada.txt","r")
+cadena = f.read()
+f.close()
+lex = Lexico(cadena)
+
